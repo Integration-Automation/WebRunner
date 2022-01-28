0fc073f7c7a9cfe7f4bf7404a5f9bc5e099768fc
JE-Chen <zenxcvwait@gmail.com>
2022-01-28 22:04:14 +0800

    refactor get driver
    refactor get driver

--- selenium_wrapper/wrapper/webdriver_manager/get_webdrivers.py
+++ selenium_wrapper/wrapper/webdriver_manager/get_webdrivers.py
@@ -1,4 +1,11 @@
+import sys
+
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.edge.service import Service
+from selenium.webdriver.ie.service import Service
+from selenium.webdriver.safari.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.opera import OperaDriverManager
@@ -15,6 +22,15 @@ webdriver_manager_dict = {
     "ie": IEDriverManager,
 }
 
+webdriver_service_dict = {
+    "chrome": webdriver.chrome.service.Service,
+    "chromium": webdriver.chrome.service.Service,
+    "firefox": webdriver.firefox.service.Service,
+    "edge": webdriver.edge.service.Service,
+    "ie": webdriver.ie.service.Service,
+    "safari": webdriver.safari.service.Service,
+}
+
 webdriver_dict = {
     "chrome": webdriver.Chrome,
     "chromium": webdriver.Chrome,
@@ -26,11 +42,20 @@ webdriver_dict = {
 }
 
 
-def get_webdriver(web_driver_name: str, **kwargs):
+def get_webdriver(web_driver_name: str, opera_path: str = None, **kwargs):
     web_driver_name = str(web_driver_name).lower()
     webdriver_value = webdriver_dict.get(web_driver_name)
     webdriver_install_manager = webdriver_manager_dict.get(web_driver_name)
-    if web_driver_name in ["edge", "ie"]:
-        return webdriver_value(webdriver_install_manager().install(), **kwargs)
+    if web_driver_name in ["opera"]:
+        opera_options = webdriver.ChromeOptions()
+        opera_options.add_argument('allow-elevated-browser')
+        opera_options.binary_location = opera_path
+        return webdriver_value(
+            executable_path=webdriver_manager_dict.get(web_driver_name)().install(), options=opera_options, **kwargs
+        )
     else:
-        return webdriver_value(executable_path=webdriver_install_manager().install(), **kwargs)
+        webdriver_service = webdriver_service_dict.get(web_driver_name)(
+            webdriver_install_manager().install(),
+            **kwargs
+        )
+        return webdriver_value(service=webdriver_service, **kwargs)
