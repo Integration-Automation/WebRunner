aac8fd8df788609684df463cf7c2d882d7423641
JE-Chen <zenxcvwait@gmail.com>
2022-05-08 02:58:02 +0800

    fix again close -> quit
    fix again close -> quit

--- test/test.py
+++ test/test.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     driver = driver_wrapper.webdriver
     driver.get("http://www.python.org")
     print(driver.title)
-    driver_wrapper.close()
+    driver_wrapper.quit()
