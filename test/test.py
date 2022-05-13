1981151bd8cde376b4406a3803899202825edf0f
JE-Chen <zenxcvwait@gmail.com>
2022-05-14 04:30:23 +0800

    update dev version
    add dev test
    update dev version

--- test/test.py
+++
@@ -1,8 +0,0 @@
-from je_web_runner import get_webdriver_manager
-
-if __name__ == "__main__":
-    driver_wrapper = get_webdriver_manager("firefox")
-    driver = driver_wrapper.current_webdriver
-    driver.get("http://www.python.org")
-    print(driver.title)
-    driver_wrapper.quit()
