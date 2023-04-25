6204f206d34d9eca9ed03daed676da6b0ac49ee5
JeffreyChen <33644111+JE-Chen@users.noreply.github.com>
2023-04-25 17:55:20 +0800

    Upload dev and stable and remove template
    Upload dev and stable and remove template

--- test/unit_test/create_project_test/WebRunner/executor/executor_one_file.py
+++
@@ -1,7 +0,0 @@
-from je_web_runner import execute_action, read_action_json
-
-execute_action(
-    read_action_json(
-        r"C:\Users\JeffreyChen\Desktop\Code_Space\WebRunner\test\unit_test\create_project_test/WebRunner/keyword/keyword1.json"
-    )
-)
