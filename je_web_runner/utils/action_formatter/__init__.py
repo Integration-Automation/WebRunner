c7a9202b7453793ee15734ac1bfd959eac46ae63
JeffreyChen <zenxcvwait@gmail.com>
2026-04-26 15:16:47 +0800

    Add deterministic action JSON formatter (canonical kwarg order)

---
+++ je_web_runner/utils/action_formatter/__init__.py
@@ -0,0 +1,14 @@
+"""Deterministic formatter for WebRunner action JSON files."""
+from je_web_runner.utils.action_formatter.formatter import (
+    ActionFormatterError,
+    format_actions,
+    format_file,
+    format_text,
+)
+
+__all__ = [
+    "ActionFormatterError",
+    "format_actions",
+    "format_file",
+    "format_text",
+]
